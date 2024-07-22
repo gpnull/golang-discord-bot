1c8f1dbd10dfe497afb8259326351521847d1657
null <gpdev1705@gmail.com>
2024-07-22 16:01:46 +0700

    .

--- cron/schedule_welcome_messages.go
+++ cron/schedule_welcome_messages.go
@@ -12,11 +12,51 @@ func ScheduleWelcomeMessages(s *discordgo.Session, welcomeChannelId string) {
 	c := cron.New(cron.WithLocation(time.FixedZone("GMT+7", 7*60*60)))
 
 	// Schedule the job to run at 0 2 4 6 8 10 12 14 16 18 20 22 GMT+7 daily
+	c.AddFunc("00 00 * * *", func() {
+		s.ChannelMessageSend(welcomeChannelId, "Welcome!")
+	})
+
+	c.AddFunc("00 02 * * *", func() {
+		s.ChannelMessageSend(welcomeChannelId, "Welcome!")
+	})
+
+	c.AddFunc("00 04 * * *", func() {
+		s.ChannelMessageSend(welcomeChannelId, "Welcome!")
+	})
+
+	c.AddFunc("00 06 * * *", func() {
+		s.ChannelMessageSend(welcomeChannelId, "Welcome!")
+	})
+
+	c.AddFunc("00 08 * * *", func() {
+		s.ChannelMessageSend(welcomeChannelId, "Welcome!")
+	})
+
+	c.AddFunc("00 10 * * *", func() {
+		s.ChannelMessageSend(welcomeChannelId, "Welcome!")
+	})
+
+	c.AddFunc("00 12 * * *", func() {
+		s.ChannelMessageSend(welcomeChannelId, "Welcome!")
+	})
+
+	c.AddFunc("00 14 * * *", func() {
+		s.ChannelMessageSend(welcomeChannelId, "Welcome!")
+	})
+
 	c.AddFunc("00 16 * * *", func() {
 		s.ChannelMessageSend(welcomeChannelId, "Welcome!")
 	})
 
-	c.AddFunc("02 16 * * *", func() {
+	c.AddFunc("00 18 * * *", func() {
+		s.ChannelMessageSend(welcomeChannelId, "Welcome!")
+	})
+
+	c.AddFunc("00 20 * * *", func() {
+		s.ChannelMessageSend(welcomeChannelId, "Welcome!")
+	})
+
+	c.AddFunc("00 22 * * *", func() {
 		s.ChannelMessageSend(welcomeChannelId, "Welcome!")
 	})
 
